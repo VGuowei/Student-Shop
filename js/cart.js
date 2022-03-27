be17ddfcd0e1d782a2087f55f350e30797d3489a
VGuowei <93118321+VGuowei@users.noreply.github.com>
2022-03-27 17:30:09 +0200

    css again

--- js/cart.js
+++ js/cart.js
@@ -6,20 +6,31 @@ if(localStorage.item){
     let cartEle = document.getElementById("cart-added");
     for(let i=0; i<arrItem.length; i++){
 
-
         cartEle.innerHTML+=
-            "<h3>"+1+"</h3>"
-            +"<div class='cart-info-item'>"
-            +"<img src='"+arrItem[i].imgSrc+"' alt='logo' class='cart-img'>"
-            +"<p>"+arrItem[i].color+"</p>"
-            +"</div>"
-            +"<h3>"+arrItem[i].type+"</h3>"
-            +"<p>"+"<b>"+arrItem[i].price+"</b>"+"</p>";
-
+            "<div class='addedRow' id='"+i+"'>"
+                +"<h3>"+1+"</h3>"
+                +"<div class='cart-info-item'>"
+                    +"<img src='"+arrItem[i].imgSrc+"' alt='logo' class='cart-img'>"
+                    +"<p>"+arrItem[i].color+"</p>"
+                +"</div>"
+                +"<h3>"+arrItem[i].type+"</h3>"
+                +"<div>"
+                    +"<p>"+"<b>"+arrItem[i].price+"</b>"+"</p>"
+                    +"<button onclick=removeItem('"+i+"')>"+"Remove"+"</button>"
+                +"</div>"
+            +"</div>";
     }
 }
+
 // Clear the basket
 function clearBasket(){
     localStorage.removeItem('item');
     document.getElementById("cart-added").innerHTML="";
+}
+
+function removeItem(id){
+    // let x=JSON.parse(localStorage.getItem("item"));
+    // console.log(x[id]);
+    // localStorage.removeItem(x[id]);
+    document.getElementById(id).innerHTML="";
 }
